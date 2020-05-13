b39c0a8f9711137adf5ee84207b9ff2052b26b84
Amandeep <sharma67@buffalo.edu>
2020-05-13 18:13:17 -0500

    testing

--- FlameSoft/fscode.py
+++ FlameSoft/fscode.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[4]:
@@ -6,75 +5,70 @@
 
 import cv2
 import numpy as np
-import timeit
 from pynput.keyboard import Key, Controller
+
 keyboard = Controller()
-import os
+
 
 class fs(object):
-    
-    def __init__(self,path):
-        self.points=[]
-        self.crop=False
-        self.path=path
-    
 
-    def area(self): 
+    def __init__(self, path):
+        self.points = []
+        self.crop = False
+        self.path = path
+
+    def area(self):
         """Crop the image"""
-        
-        def mouse(event,x,y,flags,param):
+
+        def mouse(event, x, y, flags, param):
             global refpt, crop
 
-            if event==cv2.EVENT_LBUTTONDOWN:
-                crop=True
-                self.points.append((x,y))
+            if event == cv2.EVENT_LBUTTONDOWN:
+                crop = True
+                self.points.append((x, y))
 
 
-            elif event==cv2.EVENT_LBUTTONUP:
-                crop=False
-                self.points.append((x,y))
-                
+            elif event == cv2.EVENT_LBUTTONUP:
+                crop = False
+                self.points.append((x, y))
+
                 """Alows to press and release keys"""
                 keyboard.press(Key.esc)
                 keyboard.release(Key.esc)
 
-
         cap = cv2.VideoCapture(0)
 
         cv2.namedWindow("frame")
-        cv2.setMouseCallback("frame",mouse)
-
+        cv2.setMouseCallback("frame", mouse)
 
         """Extracting Area of Interest from Image"""
 
-        img=cv2.imread(self.path)
-        red=img[:,:,2]
-        cv2.imshow('frame',red)
+        img = cv2.imread(self.path)
+        red = img[:, :, 2]
+        cv2.imshow('frame', red)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        dx=self.points[1][0]-self.points[0][0]
-        dy=self.points[1][1]-self.points[0][1]
+        dx = self.points[1][0] - self.points[0][0]
+        dy = self.points[1][1] - self.points[0][1]
         return self.points
-    
-    
-    def cropVideo(self):         
-       
+
+    def cropVideo(self):
+
         """Crop the image"""
-        
-        def mouse(event,x,y,flags,param):
+
+        def mouse(event, x, y, flags, param):
             global refpt, crop
 
+            if event == cv2.EVENT_LBUTTONDOWN:
+                crop = True
+                self.points.append((x, y))
 
-            if event==cv2.EVENT_LBUTTONDOWN:
-                crop=True
-                self.points.append((x,y))
 
+            elif event == cv2.EVENT_LBUTTONUP:
+                crop = False
+                self.points.append((x, y))
 
-            elif event==cv2.EVENT_LBUTTONUP:
-                crop=False
-                self.points.append((x,y))
-                
                 """Alows to press and release keys"""
                 keyboard.press(Key.esc)
                 keyboard.release(Key.esc)
@@ -82,18 +76,17 @@ class fs(object):
         cap = cv2.VideoCapture(self.path)
         length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         fps = (cap.get(cv2.CAP_PROP_FPS))
-        step=int(length/fps)/3
-        cap.set(cv2.CAP_PROP_POS_MSEC,step*1000)
+        step = int(length / fps) / 3
+        cap.set(cv2.CAP_PROP_POS_MSEC, step * 1000)
 
-        success,image = cap.read()
+        success, image = cap.read()
 
         cv2.namedWindow("frame")
-        cv2.setMouseCallback("frame",mouse)
-
+        cv2.setMouseCallback("frame", mouse)
 
         """Extracting Area of Interest from Image"""
-        red=image[:,:,2]
-        cv2.imshow('frame',red)
+        red = image[:, :, 2]
+        cv2.imshow('frame', red)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -103,88 +96,77 @@ class fs(object):
 # In[5]:
 
 
+class vid(object):
+    def __init__(self, path, points, filtersize, pixelBrightness):
+        self.path = path
+        self.points = points
+        self.pixelBrightness = pixelBrightness
+        self.x = []
+        self.y = []
+        self.filter = filtersize
+        self.edgemat = []
+        self.insFPS = 0
 
-class vid(object):    
-    def __init__(self,path,points,filtersize,pixelBrightness):
-        self.path=path
-        self.points=points
-        self.pixelBrightness=pixelBrightness
-        self.x=[]
-        self.y=[]
-        self.filter=filtersize
-        self.edgemat=[]
-        self.insFPS=0
-    
     def edge(self):
         cap = cv2.VideoCapture(self.path)
         length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         self.insFPS = (cap.get(cv2.CAP_PROP_FPS))
-        duration=length/self.insFPS
-        dx=self.points[1][0]-self.points[0][0] # pixels in x direction
-        dy=self.points[1][1]-self.points[0][1] #pixels in y direction
-        
-        
+        duration = length / self.insFPS
+        dx = self.points[1][0] - self.points[0][0]  # pixels in x direction
+        dy = self.points[1][1] - self.points[0][1]  # pixels in y direction
+
         ##          """ Mask for the Back Ground Substractor"""
-        #fgbg = cv2.createBackgroundSubtractorMOG2()
+        # fgbg = cv2.createBackgroundSubtractorMOG2()
 
         while True:
-            #Reading Frame
+            # Reading Frame
             ret, frame = cap.read()
 
-            if ret==False:       
+            if ret == False:
                 break
 
-
             # Extracting the clicked frame from mouse events
-            red=frame[:,:,2]
-            
-            img=red[self.points[0][1]:self.points[1][1],self.points[0][0]:self.points[1][0]]
+            red = frame[:, :, 2]
 
-            #Apply Gaussian Filter 
-            img_blur = cv2.blur(img,(self.filter,self.filter))
+            img = red[self.points[0][1]:self.points[1][1], self.points[0][0]:self.points[1][0]]
 
-            #Apply image threshholding
-            ret1,thresh=cv2.threshold(img_blur,self.pixelBrightness,255,cv2.THRESH_BINARY)
+            # Apply Gaussian Filter
+            img_blur = cv2.blur(img, (self.filter, self.filter))
 
-            #Apply the Background Substractor
-        #     fgmask = fgbg.apply(img, learningRate=0.001)      #Uncheck to activate
+            # Apply image threshholding
+            ret1, thresh = cv2.threshold(img_blur, self.pixelBrightness, 255, cv2.THRESH_BINARY)
 
-            #Apply Canny Edge Detection
-            edge=cv2.Canny(thresh,175,200) #Edge from threshholding
+            # Apply the Background Substractor
+            #     fgmask = fgbg.apply(img, learningRate=0.001)      #Uncheck to activate
 
-        #     edge=cv2.Canny(fgmask,175,255) #Edge from Backgroun Substractor Uncheck to activate
+            # Apply Canny Edge Detection
+            edge = cv2.Canny(thresh, 175, 200)  # Edge from threshholding
 
+            #     edge=cv2.Canny(fgmask,175,255) #Edge from Backgroun Substractor Uncheck to activate
 
-        ###        """ Storing the data for the  calculation"""
+            ###        """ Storing the data for the  calculation"""
 
             self.edgemat.append(edge)
-            
 
+            concat = np.vstack((img, thresh, edge))
+            cv2.imshow('Flame', concat)
 
-
-            concat=np.vstack((img,thresh,edge))
-            cv2.imshow('Flame',concat)
-
-            k=cv2.waitKey(10) & 0xff
+            k = cv2.waitKey(10) & 0xff
             # Press Q on keyboard to  exit
-            if k==27:
-              break
-    
+            if k == 27:
+                break
 
         cap.release()
         cv2.destroyAllWindows()
-        
+
         return self.edgemat
-    
-                
 
 
 # In[7]:
 
 
 if __name__ == "__main__":
-    crop=fs('A01.avi')
-    a=fs('A01.avi').cropVideo()
+    crop = fs('A01.avi')
+    a = fs('A01.avi').cropVideo()
 # #     crop=[(91, 356), (969, 570)]
 #     aa=vid('A01.avi',a,75,150).edge()
-
